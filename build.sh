76a38ef18a2a5461115e3b872444e24e7620271f
Haz_01 <haz01@hazonline.co.uk>
2020-04-28 15:43:35 +0100

    Better installer
    improved build.sh to convert deb to rpm, slp, lsb and tgz.

--- build.sh
+++ build.sh
@@ -1,25 +1,29 @@
 #!/bin/sh
 {
-	echo "[0/7] - Concatinating python source"
+	echo "[0/9] - Concatinating python source"
 	cat src/menu.py src/config.py src/tclock.py > src/main.py
-	echo "[1/7] - Copying python source"
+	echo "[1/9] - Copying python source"
 	cp -r src/* tclock-deb/usr/share/tclock/
 	cp -r src/* install/usr/share/tclock/
-	echo "[2/7] - deleting debug flag"
+	echo "[2/9] - deleting debug flag"
 	rm tclock-deb/usr/share/tclock/debug
 	rm install/usr/share/tclock/debug
-	echo "[3/7] - making python executable"
+	echo "[3/9] - making python executable"
 	chmod 777 tclock-deb/usr/share/tclock/main.py
 	chmod 777 install/usr/share/tclock/main.py
-	echo "[4/7] - Packaging For Debian"
+	echo "[4/9] - Packaging For Debian"
 	dpkg --build tclock-deb
 	
-	echo "[5/7] - Compressing Install.zip"
+	echo "[5/9] - Compressing Install.zip"
     zip -r9 build/tclock.zip ./install/
-    echo "[6/7] - Copying last files to build folder"
+    echo "[6/9] - Copying last files to build folder"
 	mv tclock-deb.deb build/tclock.deb
 	cp LICENSE build/LICENSE
-	echo "[7/7] - Done"
+	echo "[7/9] Conveting deb to rpm, slp, lsb and tgz"
+	sudo alien --to-rpm tclock.deb;sudo alien --to-slp tclock.deb;sudo alien --to-lsb tclock.deb; sudo alien --to-tgz tclock.deb
+	echo "[8/9] Taking ownership over all files"
+	sudo chown harry:harry ./*
+	echo "[9/9] - Done"
 }||{
 	echo "Failed"
 }
